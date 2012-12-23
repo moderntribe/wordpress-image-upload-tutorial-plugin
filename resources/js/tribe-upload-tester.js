453ea2084eda27c66569bf4c27ace2990819cbc2
Peter Chester <peter@tri.be>
2012-12-22 20:25:20 -0800

    Implement js notes from http://codecanyon.net/forums/thread/wordpress-35-media-uploader-api/83117
    Signed-off-by: Peter Chester <peter@tri.be>

--- resources/js/tribe-upload-tester.js
+++ resources/js/tribe-upload-tester.js
@@ -1,60 +1,45 @@
 jQuery(document).ready(function($){
 
-	_my_media = wp.media;
+	// Define uploader settings
+	var frame = wp.media({
+		title : 'Tribe Image Upload Test',
+		multiple : false,
+		library : { type : 'image' },
+		button : { text : 'Insert into Tribe Test Screen' }
+	});
 
 	// Handle results from media manager.
-	_my_media.editor.send.attachment = function(props, attachment) {
-		render_image(props, attachment);
-	}
+	frame.on('close',function( ) {
+		var attachment = frame.state().get('selection').first().toJSON();
+		tribe_render_image(attachment);
+	});
 
 	// Hook into upload button.
 	$('.uploader .button').click(function(e) {
-		_my_media.editor.open($(this));
+		frame.open();
 		return false;
 	});
 
 	// Output selected image HTML.
 	// This part could be totally rewritten for your own purposes to process the results!
-	render_image = function(props, attachment) {
-		console.log(props);
+	tribe_render_image = function(attachment) {
+
 		console.log(attachment);
 
-		var img_url = attachment.sizes[props.size].url;
-		var img_width = attachment.sizes[props.size].width;
-		var img_height = attachment.sizes[props.size].height;
-
 		// Generate IMG
-		img_html = '<img src="' + img_url + '" ';
-		img_html += 'width="' + img_width + '" ';
-		img_html += 'height="' + img_height + '" ';
+		img_html = '<img src="' + attachment.url + '" ';
+		img_html += 'width="' + attachment.width + '" ';
+		img_html += 'height="' + attachment.height + '" ';
 		if ( attachment.alt != '' ) {
 			img_html += 'alt="' + attachment.alt + '" ';
 		}
-		if ( props.align != 'none' ) {
-			img_html += 'class="align' + props.align + '" ';
-		}
 		img_html += '/>';
 
-		// Generate link.
-		var img_link_url = false;
-		if ( props.link && props.link != 'none' ) {
-			if ( props.link == 'custom' ) {
-				img_link_url = props.img_link_url;
-			} else if ( props.link == 'file' ) {
-				img_link_url = attachment.url;
-			} else if ( props.link == 'post' ) {
-				img_link_url = attachment.link;
-			}
-
-			var img_link_html = '<a href="' + img_link_url + '" ';
-			if ( attachment.title != '' ) {
-				img_link_html += 'title="' + attachment.title + '"';
-			}
-			img_link_html += '>' + img_html + '</a>';
-			img_html = img_link_html;
-		}
-
 		$("#tribe_preview").html(img_html);
+
+		// if you save this you will probably want to also verify the nonce for security (attachment.nonces.update).
+		//console.log(attachment.nonces.update);
+
 	}
 
 });
